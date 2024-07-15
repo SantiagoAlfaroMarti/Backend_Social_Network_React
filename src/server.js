f2849d59637a59c156edcd0815ef268bd33f4840
unknown <santiagoalfamar@gmail.com>
2024-07-15 17:38:14 +0200

    create server.js and start server

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import 'dotenv/config';
+import { dbConnection } from './database/db.js';
+import { router as gamesRoutes } from './router.js';
+
+const app = express();
+app.use(express.json())
+const PORT = process.env.PORT || 5001;
+
+app.get('/healthy', (req, res) => {
+    res.json({
+        success: true,
+        message: "Server is healthy!"
+    });
+});
+app.use('/api', gamesRoutes)
+
+dbConnection()
+    .then(() => {
+        console.log('Database connection established!');
+        app.listen(PORT, () => {
+            console.log(`Server running on ${PORT}`);
+        });
+    })
+    .catch(error => {
+        console.error('Error establishing connection with the database:', error);
+    });
