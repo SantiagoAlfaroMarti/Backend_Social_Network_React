2044157c656d73b04df67ddc51134f1d904bdc62
unknown <santiagoalfamar@gmail.com>
2024-09-02 21:41:58 +0200

    update: update backend with frontend

--- src/server.js
+++ src/server.js
@@ -2,8 +2,10 @@ import express from 'express';
 import 'dotenv/config';
 import { dbConnection } from './database/db.js';
 import router from './router.js';
+import cors from 'cors';
 
 const app = express();
+app.use(cors())
 app.use(express.json())
 
 const PORT = process.env.PORT
